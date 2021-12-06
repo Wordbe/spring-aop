aaadbf6186e2042e1634df0bbcaa205382b771e6
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-06 22:58:21 +0900

    feat: AOP 테스트 생성, 포인트컷 분리

---
+++ src/main/java/co/wordbe/springaop/order/OrderRepository.java
@@ -0,0 +1,28 @@
+package co.wordbe.springaop.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    public void save(String itemId) {
+        log.info("[orderRepository] 실행");
+        // 저장 로직
+        if ("ex".equals(itemId)) {
+            throw new IllegalStateException("예외 발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log.error("", e);
+        }
+    }
+}
---
+++ src/main/java/co/wordbe/springaop/order/aop/AspectV1.java
@@ -0,0 +1,17 @@
+package co.wordbe.springaop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+
+    @Around("execution(* co.wordbe.springaop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[doLog] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
