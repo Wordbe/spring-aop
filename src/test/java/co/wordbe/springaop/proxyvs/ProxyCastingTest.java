e6a294ff9f844d8713f84af54fffa7fbcc6447ef
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2021-12-08 23:44:38 +0900

    feat: 프록시를 타겟으로 타입 캐스팅(JDK 실패, CGLIB 성공)

---
+++ src/test/java/co/wordbe/springaop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,42 @@
+package co.wordbe.springaop.proxyvs;
+
+import co.wordbe.springaop.order.member.MemberService;
+import co.wordbe.springaop.order.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberServiceProxy = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // CGLIB 동적 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberServiceProxy = (MemberServiceImpl) memberServiceProxy;
+
+    }
+}
